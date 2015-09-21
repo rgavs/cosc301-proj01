b9b1fa17bf101e48b796c0540e5abcf1f75fdb2e
Ryan Gavigan <gavigan.ryan@gmail.com>
2015-09-21 01:32:01 -0400

    RC1: code clean up

--- hangperson.c
+++ hangperson.c
@@ -8,28 +8,18 @@
 
 // Ryan Gavigan
 // COSC 301
-// 09/05/15
+// 09/20/15
 // Project 1
 
 /*
- * Linked list data structure definitions.  You can choose
- * to use these data structure and type definitions or modify
- * them to suit your taste.
+ * Linked list data structure.
  */
 struct _wordnode {
     char word[256];
     struct _wordnode *next;
-};
-typedef struct _wordnode wordnode;
+}; typedef struct _wordnode wordnode;
 
-/*
- * Be happy you didn't have to write this function.
- * Prints a low-tech ASCII gallows.  Max number of misses
- * is 7 (i.e., if there are 7 misses, the player loses and
- * the poor sap gets hung).
- */
 void print_gallows(int num_missed) {
-    // make sure that we got called in a reasonable way
     assert(num_missed >= 0 && num_missed <= 7);
 
     printf("\n\n       |||========|||\n");
@@ -73,23 +63,22 @@ void print_gallows(int num_missed) {
 }
 
 /*
- * Play one game of Hangperson.  The secret word is passed as a
- * parameter.  The function should return true if the player won,
+ * Play one game of Hangperson. The secret word is passed as a
+ * parameter. The function should return true if the player won,
  * and false otherwise.
  */
 bool one_game(const char *word) {
-    int num_guesses = 0;
-	int num_missed = 0;
+    int num_guesses = 0, num_missed = 0;
 	int left = (int) strlen(word);	// num chars left to guess
 	bool correct = false; 			// true if current guess is in word, else false
-    char stat[left * 2];
-    stat[left * 2] = '\0';
+    char status[left * 2];
+    status[left * 2] = '\0';
     for(int i = 0; i < left * 2; i++){
     	if(i % 2 == 0)
-	    	stat[i] = '_';
+	    	status[i] = '_';
 	    else
-	    	stat[i] = ' ';
-    }	// loop initializes array, alternating chars for readability
+	    	status[i] = ' ';
+    }	// loop initializes array, alternating spaces/underscores for readability
     char guessed[32] = {'\0'};
     while(num_missed < 7 && left > 0){
         correct = false;
@@ -98,9 +87,9 @@ bool one_game(const char *word) {
 	    print_gallows(num_missed);
 		char guess[128] = {'!'};
 		char c = '!';
-	    printf("     %s\n\n",stat);
+	    printf("     %s\n\n",status);
 		bool again = false;
-		do{ // char validation
+		do{ // char validation loop
             while(!isalpha(c)){
 				printf("Please guess a letter: ");
 				fgets(guess, 128, stdin);
@@ -114,8 +103,8 @@ bool one_game(const char *word) {
                 }
                 if(c == guessed[k]){
 					again = true;
-					printf("Please guess a letter you have not already guessed.\n");
 					printf("You have guessed: \'%s\'\n",guessed);
+					printf("Please guess a letter you have not already guessed.");
 					fgets(guess, 8, stdin);
 					c = toupper(guess[0]);
 				}
@@ -128,7 +117,7 @@ bool one_game(const char *word) {
 		} while(again);
 		for(int x = 0; x < strlen(word); x++){
 	    	if(word[x] == c){
-	    		stat[x * 2] = c;
+	    		status[x * 2] = c;
 	    		correct = true;
 	    		left--;
 	    	}
@@ -137,7 +126,7 @@ bool one_game(const char *word) {
 	    	num_missed++;
     }
     print_gallows(num_missed);
-	printf("     %s\n\n",stat);
+	printf("     %s\n\n",status);
     if(left == 0){
     	printf("Congratulations! You have won!\n\n");
     	return true;
@@ -149,31 +138,23 @@ bool one_game(const char *word) {
 /*
  * wordbank-related functions. please don't change the
  * function prototypes for these three functions.
- * load_words takes the name of the file that should be
- * opened and words read from, and a pointer to an int
- * that should be indirectly modified to store the number
- * of words loaded from the file.  The function should
- * return the linked list of words.
  */
 wordnode *load_words(const char *filename, int *num_words) {
     FILE* file = fopen(filename, "r");
-    wordnode * head = malloc(sizeof(wordnode));
-   	wordnode * line = malloc(sizeof(wordnode));
+    wordnode * head = malloc(sizeof(wordnode)), * line = malloc(sizeof(wordnode));
     head->next = line;
-    int i = 0;
+    int i;
     char str[256];
     while(!feof(file)){
     	i = 0;
     	fgets(str, sizeof(str), file);
     	strtok(str," \t\n");
-    	if(strlen(str) == 0)
-        	break;
-        for(; i < strlen(str); i++){
+        for(; i < strlen(str); i++){    // testing isalpha and setting toupper
             if(!isalpha(str[i]))
                 break;
 	        str[i] = toupper(str[i]);
         }
-        if(i == strlen(str)){
+        if(i == strlen(str)){           // one-time step to place a word into head->word
 	    	(*num_words)++;
         	if(*num_words == 1)
         		strcpy(head->word,str);
@@ -185,15 +166,14 @@ wordnode *load_words(const char *filename, int *num_words) {
 		    }
         } // end of if
     } // end of while
-   	line = NULL;
+   	line = NULL;    // ensures line does not point to a node in the list
     free(line);
     fclose(file);
     return head;
 }
 
 void free_words(wordnode *wordlist) {
-    wordnode * node = malloc(sizeof(wordnode));
-    wordnode * tmp = malloc(sizeof(wordnode));;
+    wordnode * node = malloc(sizeof(wordnode)), * tmp = malloc(sizeof(wordnode));
     while(node != NULL){
         tmp = node->next;
         free(node);
@@ -214,12 +194,6 @@ const char *choose_random_word(wordnode *wordlist, int num_words) {
     return node->word;
 }
 
-
-/*
- * The main function is where everything starts.  Choose a random
- * word and call one_game.  Start first on getting the word-loading
- * to work correctly, then handle the game playing capabilities.
- */
 #ifndef AUTOTEST
 int main() {
     srandom(time(NULL));
